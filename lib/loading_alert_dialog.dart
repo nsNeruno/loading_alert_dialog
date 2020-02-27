58b529f3d4ef6628e2bde4b41adf3a9363a50b13
neruno <infinia249@gmail.com>
2020-02-27 12:00:06 +0700

    Removed GlobalKey based Navigator control

--- lib/loading_alert_dialog.dart
+++ lib/loading_alert_dialog.dart
@@ -7,12 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 ///
-/// Signature for building a [Widget] like the common builder of [showDialog]
-/// which also provides access to [_loadingKey].
-///
-typedef KeyedWidgetBuilder = Widget Function(BuildContext context, Key key);
-
-///
 /// Signature for callback that indicates the computation has completed and the
 /// dialog should pop with a result.
 ///
@@ -26,33 +20,12 @@ typedef DialogPopCallback<T> = void Function(T result);
 typedef ErrorPopCallback = void Function(dynamic err);
 
 ///
-/// Single [GlobalKey] to provide [BuildContext] so the dialog may be popped.
-/// This key is provided on [KeyedWidgetBuilder].
-///
-GlobalKey _loadingKey = GlobalKey();
-
-///
-/// Takes the [Widget] from the [KeyedWidgetBuilder], and returns it.
-/// The widget must have [_loadingKey] attached to it, or else the dialog may
-/// not be pop-able.
-///
-Widget _buildWidget(BuildContext context, KeyedWidgetBuilder builder) {
-  final widget = builder(context, _loadingKey);
-  assert(widget.key == _loadingKey);
-  return widget;
-}
-
-///
 /// Displays a platform aware dialog that either calls [showCupertinoDialog]
 /// or [showDialog] depending on current [Platform], and runs indefinitely,
 /// while showing a [Widget].
 ///
 /// Takes a [BuildContext] as primary requirements for common dialog showing
 /// methods.
-///
-/// The [KeyedWidgetBuilder] works like normal [WidgetBuilder], but also
-/// provides a [GlobalKey] to be included inside the displayed widget, enabling
-/// access to it's context.
 ///
 /// During the [computation], the [DialogPopCallback] and [ErrorPopCallback] are
 /// provided to control when to pop the dialog and what should be popped from
@@ -73,7 +46,7 @@ Widget _buildWidget(BuildContext context, KeyedWidgetBuilder builder) {
 ///
 void showLoadingDialog<T>({
   @required BuildContext context,
-  @required KeyedWidgetBuilder builder,
+  @required WidgetBuilder builder,
   void Function( DialogPopCallback<T> pop, ErrorPopCallback error, ) computation,
   void Function(T result) onPop,
   void Function(dynamic error) onError,
@@ -92,13 +65,13 @@ void showLoadingDialog<T>({
   if (Platform.isIOS && isPlatformAware == true) {
     showCupertinoDialog<T>(
       context: context,
-      builder: (context) => _buildWidget(context, builder),
+      builder: builder,
     ).then(dialogCallback);
   } else {
     showDialog<T>(
       barrierDismissible: false,
       context: context,
-      builder: (context) => _buildWidget(context, builder),
+      builder: builder,
     ).then(dialogCallback);
   }
 
@@ -106,13 +79,13 @@ void showLoadingDialog<T>({
     // Checking hasPopped flag ensures only one Navigator.pop event
     final Function pop = (T result) {
       if (hasPopped) { return; }
-      Navigator.of(_loadingKey.currentContext).pop(result);
+      Navigator.of(context).pop(result);
       hasPopped = true;
     };
 
     final Function error = (err) {
       if (hasPopped) { return; }
-      Navigator.of(_loadingKey.currentContext).pop();
+      Navigator.of(context).pop();
       hasPopped = true;
       if (onError != null) {
         onError(err);
